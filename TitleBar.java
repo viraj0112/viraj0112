ecb8a20e9555ed94a384e002657abba78c3fa9c2
viraj0112 <83603367+viraj0112@users.noreply.github.com>
2021-11-26 11:40:23 +0530

    Update TitleBar.java

--- TitleBar.java
+++ TitleBar.java
@@ -2,15 +2,30 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class TitleBar extends JPanel{
-
+public class TitleBar extends JPanel implements ActionListener  {
+	JButton settingButton = new JButton("Setting");
+	
 	TitleBar(){
 		this.setPreferredSize(new Dimension(500,80));
+		this.add(settingButton);
+		
 		JLabel titleText= new JLabel("StudentApp");
 		titleText.setPreferredSize(new Dimension(300,75));
 		titleText.setFont(new Font("Manjari",Font.BOLD, 30));
 		titleText.setHorizontalAlignment(JLabel.CENTER);
 		this.add(titleText);
+		
+		settingButton.setBounds(100, 160, 200, 40);
+		settingButton.setFocusable(false);
+		settingButton.addActionListener(this);
+		
 	}
 	
+	@Override
+		public void actionPerformed(ActionEvent e){
+			if (e.getSource()== settingButton) {
+				
+				NewWindow newwindow = new NewWindow();
+			} 
+		}
 }
